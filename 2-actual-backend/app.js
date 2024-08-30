d91372b67e515abeb68ca119b64ff290fefc9c44
Aman Poddar <amanpoddar2019@gmail.com>
2024-08-31 00:09:06 +0545

    app.js change

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -8,11 +8,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://myntra-clone-frontend-five.vercel.app/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+
+// // Serve static files from the React frontend app
+// app.use(express.static(path.join(__dirname, "../3-myntra-react-clone/build")));
 
 app.get("/items", async (req, res) => {
   const storedItems = await getStoredItems();
@@ -36,6 +43,12 @@ app.post("/items", async (req, res) => {
   const updatedItems = [newItem, ...existingItems];
   await storeItems(updatedItems);
   res.status(201).json({ message: "Stored new item.", item: newItem });
+});
+// All other GET requests not handled by API routes return the React app
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../3-myntra-react-clone/build", "index.html")
+  );
 });
 
 app.listen(8080);
